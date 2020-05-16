883c8edd25c08536f4d1a823a6e924b4f0c65149
Daniel Armstrong <33160370+armstrongdj@users.noreply.github.com>
2020-05-15 20:05:32 -0700

    Fixed issue #1 - guess count bug

--- Hilo.py
+++ Hilo.py
@@ -11,20 +11,21 @@ while True:
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Enter h or l, or c if my guess was correct "
                      .format(guess)).casefold()
-
+    
     if high_low == "h":
         # Guess higher. The low end of the range becomes 1 greater than the guess.
         low = guess + 1
+        guesses += 1
     elif high_low == "l":
         # Guess lower. The high end of the range becomes 1 less than the guess.
         high = guess - 1
+        guesses += 1
     elif high_low == "c":
         print("I got it in {} guesses!".format(guesses))
         break
     else:
         print("Please enter h, l, or c")
-    # guesses = guesses + 1
-    guesses += 1
+        
 
 if guesses <= 10:
     print("I win!")
