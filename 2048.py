61b3b1e366c5446a22050d8cf90ded13afc96040
Daniel Armstrong <33160370+armstrongdj@users.noreply.github.com>
2020-05-20 21:49:32 -0700

    Changed 2048 to allow automatic strategies

--- 2048.py
+++ 2048.py
@@ -1,14 +1,13 @@
 import random
 
-rand = random.Random()
-rand.seed(1)
-
 def get_new_board():
     return [[0]*4, [0]*4, [0]*4, [0]*4]
 
 def print_board(board):
     for row in board:
         print('{} {} {} {}'.format(*['{: 4d}'.format(n) if n > 0 else '   _' for n in row]))
+    print('--------------------------------')
+
 
 def get_available(board):
     return [(r,c) for r in range(0, 4) for c in range(0, 4) if board[r][c] == 0]
@@ -17,8 +16,8 @@ def add_number(board):
     available = get_available(board)
     if not available:
         raise Exception('No tiles left')
-    number = rand.randint(1, 2) * 2
-    location = available[rand.randrange(0, len(available))]
+    number = random.randint(1, 2) * 2
+    location = available[random.randrange(0, len(available))]
     board[location[0]][location[1]] = number
 
 def shift_numbers(numbers):
@@ -75,7 +74,38 @@ def shift_board(board, direction):
         
     return new_board
 
-def play_game():
+def manual_strategy(last_move_changed_board):
+    return input('Direction (U)p, (D)own, (L)eft, (R)ight, or (Q)uit? ').strip().casefold()
+
+auto1_state = {'index': 0}
+
+def auto_strategy1(last_move_changed_board):
+    moves = ['u', 'r', 'd', 'l']
+    index = auto1_state['index']
+    auto1_state['index'] = (index + 1) % 4
+    return moves[index]
+
+auto2_state = {'index': 0}
+
+def auto_strategy2(last_move_changed_board):
+    moves = ['d', 'r', 'l', 'u']
+    index = auto2_state['index']
+    if not last_move_changed_board:
+        auto2_state['index'] = (index + 1) % 4
+    else:
+        auto2_state['index'] = 0
+    return moves[index]
+
+auto3_state = {'index': 0}
+
+def auto_strategy3(last_move_changed_board):
+    moves = ['u', 'r', 'd', 'l', 'u']
+    index = auto3_state['index']
+    auto3_state['index'] = (index + 1) % 4
+    return moves[index]
+
+
+def play_game(strategy = manual_strategy, show_board = True):
     board = get_new_board()
     add_number(board)
 
@@ -88,14 +118,17 @@ def play_game():
             board == shift_board(board, 'l') and \
             board == shift_board(board, 'u') and \
             board == shift_board(board, 'd'):
-            print('No moves. Game over.')
+            if show_board:
+                print('No moves. Game over.')
             break
         
         if available and changed:            
             add_number(board)
-        print_board(board)
 
-        direction = input('Direction (U)p, (D)own, (L)eft, (R)ight, or (Q)uit? ').strip().casefold()
+        if show_board:
+            print_board(board)
+
+        direction = strategy(changed)
 
         if direction == 'q':
             break
@@ -107,8 +140,31 @@ def play_game():
         previous_board = board
         board = shift_board(board, direction)
         changed = board != previous_board
+    return board
         
 
+def test_strats(strategies, games):
+    counter = 0
+    stats = {}
+    for strategy in strategies:
+        counter += 1
+        name = 'Strategy {}'.format(counter)
+        max_highest = 0
+        for game_number in range(0, games):
+            board = play_game(strategy, show_board=False)
+            flat_board = []
+            for row in board:
+                flat_board.extend(row)
+            highest = max(flat_board)
+            max_highest = max(highest, max_highest)
+        stats[name] = max_highest
+    return stats
+
+def quick_test(games):
+    strats = [auto_strategy1, auto_strategy2, auto_strategy3]
+    stats = test_strats(strats, games)
+    print(stats)
 
 if __name__ == "__main__":
+    #quick_test(10)
     play_game()
